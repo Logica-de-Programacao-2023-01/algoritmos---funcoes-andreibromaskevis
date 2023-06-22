3d4daa4137ae3909ab61a45b8e89b422516fe770
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 19:15:35 -0300

    Função_18.go

---
+++ Função_18.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+func APFUNC(slice []int, fn func(int) int) (int, error) {
+	if len(slice) == 0 {
+		return 0, errors.New("Slice vazio")
+	}
+	resultado := 0
+	for _, valor := range slice {
+		resultado += fn(valor)
+	}
+	return resultado, nil
+}
+func dobrar(valor int) int {
+	return valor * 2
+}
+func main() {
+	slice := []int{1, 2, 3, 4, 5}
+	resultado, err := APFUNC(slice, dobrar)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(resultado)
+}
