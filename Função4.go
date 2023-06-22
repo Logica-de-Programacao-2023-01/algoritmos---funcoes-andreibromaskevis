b2e918a832788c130db4de27fdf083cbd43c047e
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 19:09:31 -0300

    Função4.go

---
+++ Função4.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+func segundoMaiorValor(nums []int) (int, error) {
+	if len(nums) < 2 {
+		return 0, fmt.Errorf("O slice deve ter pelo menos dois elementos")
+	}
+	sort.Ints(nums)
+	return nums[len(nums)-2], nil
+}
+func main() {
+	slice := []int{10, 5, 8, 12, 3}
+	segundoMaior, err := segundoMaiorValor(slice)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(segundoMaior)
+	}
+}
