81ae0a1c2a0ac39ffc239aec104c78fb25e37833
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 19:12:21 -0300

    Função_11.go

---
+++ Função_11.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+func isPrimo(num int) (bool, error) {
+	if num < 0 {
+		return false, errors.New("Número negativo não é válido")
+	}
+	if num < 2 {
+		return false, nil
+	}
+	for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
+		if num%i == 0 {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+func main() {
+	numero := 17
+	primo, err := isPrimo(numero)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if primo {
+		fmt.Println(numero, "é um número primo")
+	} else {
+		fmt.Println(numero, "não é um número primo")
+	}
+}
+
